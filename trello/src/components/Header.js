9ca5c49156bd1dce1aacf5e0a8fc0d939d4f6057
mirrorhanyu <mirrorhanyu@gmail.com>
2017-11-14 22:46:56 +0530

    redesign for title bar

--- trello/src/components/Header.js
+++ trello/src/components/Header.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import Typed from 'typed.js';
 import './css/Header.css';
 
 class Header extends Component {
+  
+  componentDidMount() {
+    const options = {
+      strings: [
+        "Rain falls, waves rise.",
+        "Autumn comes, summer dies.",
+        "Petals weak, flowers hide.",
+        "Time passes, love thrives.",
+        "<strong>Home</strong>^10000"
+      ],
+      loop: true,
+      typeSpeed: 50,
+      backSpeed: 50,
+      startDelay: 700,
+      backDelay: 1300
+    };
+    this.typed = new Typed(this.refs["type-content"], options);
+  }
+
+  componentWillUnmount() {
+    this.typed.destroy();
+  }
 
   render() {
     return (
       <div className="header">
-        <div className="header-title" data-text="Mirror">Mirror</div>
+        <div className="header-title">
+          <span className="header-content" ref="type-content"/>
+        </div>
       </div>
     );
   }
